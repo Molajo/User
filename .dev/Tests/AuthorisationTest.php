b2e94878888f2a1cc6d6aa1ef61f2cf9a54b766f
AmyStephen <AmyStephen@gmail.com>
2014-06-17 06:15:03 -0500

    Test stub

--- .dev/Tests/AuthorisationTest.php
+++ .dev/Tests/AuthorisationTest.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Authorisation Test
+ * Authentication Test
  *
  * @package    Molajo
  * @copyright  2014 Amy Stephen. All rights reserved.
@@ -9,97 +9,35 @@
 namespace Molajo\Tests;
 
 /**
- * Test User Authorisation
+ * Test User Authentication
  *
  * @package    Molajo
  * @license    http://www.opensource.org/licenses/mit-license.html MIT License
  * @copyright  2014 Amy Stephen. All rights reserved.
  * @since      1.0.0
  */
-class AuthorisationTest extends \PHPUnit_Framework_TestCase
+class AuthenticationTest extends \PHPUnit_Framework_TestCase
 {
     /**
-     * @var $PermissionClass
+     * @var $authentication
      */
-    protected $PermissionClass;
-
-    /**
-     * @var $AuthorisationClass
-     */
-    protected $AuthorisationClass;
+    protected $authentication;
 
     /**
      * Set up
      */
     public function setUp()
     {
-        $class                 = 'Molajo\\Permissions\\Permissions';
-        $this->Permissionclass = new $class;
-
-        $class                    = 'Molajo\\User\\Authorisation\\Authorisation';
-        $this->Authorisationclass = new $class($this->PermissionClass);
-
         return;
     }
 
     /**
-     * @covers Molajo\User\Authorisation\Authorisation::verifyLogin
+     * Set up
      */
-    public function testVerifyLogin()
+    public function testLogin()
     {
-        $id                 = 1;
-        $request            = array();
-        $request['user_id'] = $id;
-        $request['method']  = 'login';
-
-        $results = $this->AuthorisationClass->isAuthorised($request);
-
-        $this->assertEquals(true, $results);
-    }
-
-    /**
-     * @covers Molajo\User\Authorisation\Authorisation::verifyActionPermissions
-     */
-    public function testVerifyTask()
-    {
-        $request               = array();
-        $request['method']     = 'task';
-        $request['action']     = 'view';
-        $request['catalog_id'] = 1;
-
-        $results = $this->AuthorisationClass->isAuthorised($request);
-
-        $this->assertEquals(true, $results);
-    }
-
-    /**
-     * @covers Molajo\User\Authorisation\Authorisation::verifyActionPermissions
-     */
-    public function testVerifyActionTest()
-    {
-        $request                   = array();
-        $request['method']         = 'action';
-        $request['view_group_id']  = 1;
-        $request['request_action'] = 'read';
-        $request['catalog_id']     = 1;
-
-        $results = $this->AuthorisationClass->isAuthorised($request);
-
-        $this->assertEquals(true, $results);
-    }
-
-    /**
-     * @covers Molajo\User\Authorisation\Authorisation::isUserAuthorisedNoFiltering
-     */
-    public function testSetHTMLFilter()
-    {
-        $request           = array();
-        $request['method'] = 'htmlfilter';
-        $request['key']    = 'MolajoAuthorisation';
-
-        $results = $this->AuthorisationClass->isAuthorised($request);
-
-        $this->assertEquals(true, $results);
+        $this->assertEquals(1, 1);
+        return;
     }
 
     /**
